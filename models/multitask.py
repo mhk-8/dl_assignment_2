065398b42c2e7dd8fc38d312986794734a5e0f3e
Hari Krishna <majjiharikrishna2002@gmail.com>
2026-04-08 18:44:47 +0530

    gdrive IDs updated

--- models/multitask.py
+++ models/multitask.py
@@ -22,9 +22,9 @@ class MultiTaskPerceptionModel(nn.Module):
         """
         super().__init__()
         import gdown
-        gdown.download(id="1SavcaGEHFJZf00lyDr18fAhXBRCIUCM5", output=classifier_path, quiet=False)
-        gdown.download(id="1gadUcsS3Bww1tick3X70ErI15xc1FiI1", output=localizer_path, quiet=False)
-        gdown.download(id="1Ji-sgJ7HNFy0b9grbzDFUU_c3lRD-8L3", output=unet_path, quiet=False)
+        gdown.download(id="1uT6w5ZGImFIP1jYYqnYaY15XlrzpPT4x", output=classifier_path, quiet=False)
+        gdown.download(id="1DebcCf1dVhQ90aafH_5X55AmAwb7-DCC", output=localizer_path, quiet=False)
+        gdown.download(id="1XXi2G4fA5RQ9YlVOP7bVcymlaPRu5-N1", output=unet_path, quiet=False)
         # Shared Backbone
         self.encoder = VGG11(in_channels=in_channels)
                 
