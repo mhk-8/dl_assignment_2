3777d71a76b30ed0d9888284501627ca10e9a02b
Hari Krishna <majjiharikrishna2002@gmail.com>
2026-04-09 19:50:09 +0530

    new notebook update

--- models/multitask.py
+++ models/multitask.py
@@ -22,9 +22,9 @@ class MultiTaskPerceptionModel(nn.Module):
         """
         super().__init__()
         import gdown
-        gdown.download(id="1DB83O3KYIu0TWa2FCB7unfB6ge-8-gUY", output=classifier_path, quiet=False)
-        gdown.download(id="10kmNpPlXswYe0faWasSIA9rWXxLcS-GG", output=localizer_path, quiet=False)
-        gdown.download(id="1JlzkeV8cRjSfJtnpgoDwyaiEiJHeaH0Y", output=unet_path, quiet=False)
+        gdown.download(id="1RkRrb-1o3Wl9CqvghP80pplhcCuJEivE", output=classifier_path, quiet=False)
+        gdown.download(id="1ezBINXfOJF5OEpYaB7nBbiHnZoKOjRR1", output=localizer_path, quiet=False)
+        gdown.download(id="1ZjxI1j-FS2UlgMJPAxXofuSh_TtLPn_d", output=unet_path, quiet=False)
         # Shared Backbone
         self.encoder = VGG11(in_channels=in_channels)
                 
